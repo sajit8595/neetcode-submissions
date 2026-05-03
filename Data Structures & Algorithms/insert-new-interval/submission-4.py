be00793ab0a6f4ef0a50eeaf21c3c833a6649884
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-03 15:14:16 +0530

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = [intervals[0]]
+        for i in range(1, len(intervals)):
+            s1, e1 = ans[-1]
+            s2, e2 = intervals[i]
+            if s2 <= e1:
+                ans[-1] = [s1, max(e1, e2)]
+            else:
+                ans.append([s2, e2])
+        return ans
+
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(len(intervals)):
+            s1, e1 = intervals[i]
+            s2, e2 = newInterval
+            if s2 > e1:
+                # new dont overlap
+                ans.append(intervals[i])
+            elif e2 < s1:
+                # new dont overlap but new comes first
+                ans.append(newInterval)
+                return ans + intervals[i:]
+            else:
+                # new overlaps - update new
+                newInterval = [min(s1, s2), max(e1, e2)]
+        ans.append(newInterval)
+        return ans
+
