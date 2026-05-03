95023936f2fa4fbd8c71f6b3655e0fdb6d34a498
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-03 15:06:45 +0530

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = [intervals[0]]
+        for i in range(1, len(intervals)):
+            s1, e1 = ans[-1]
+            s2, e2 = intervals[i]
+            if s2 <= e1:
+                ans[-1] = [s1, max(e1, e2)]
+            else:
+                ans.append([s2, e2])
+        return ans
+
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        newIntervals = []
+        isInserted = False
+        for s, e in intervals:
+            if newInterval[0] < s:
+                newIntervals.append(newInterval)
+                isInserted = True
+            newIntervals.append((s, e))
+        if isInserted == False:
+            newIntervals.append(newInterval)
+        
+        return self.merge(newIntervals)
+
+        
