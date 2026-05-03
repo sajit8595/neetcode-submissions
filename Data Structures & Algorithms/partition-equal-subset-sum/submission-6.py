d2b51a3965973cb439a7182ba1d3b161cdee3957
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-03 16:35:35 +0530

    Add: partition-equal-subset-sum - submission-6

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-6.py
@@ -0,0 +1,43 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        summ = sum(nums)
+        if summ & 1:
+            return False
+        target = summ // 2
+
+        # def achieve(ind, tar):
+        #     if tar == 0:
+        #         return True
+        #     if ind == 0:
+        #         return tar == nums[ind]
+
+        #     dontTake = achieve(ind-1, tar)
+        #     take = False
+        #     if tar >= nums[ind]:
+        #         take = achieve(ind-1, tar-nums[ind])
+        #     return dontTake or take
+
+        # return achieve(n-1, target)
+
+        pdp = [False for t in range(target+1)]
+
+        tar = 0
+        for ind in range(n):
+            pdp[tar] = True
+        ind = 0
+        if nums[ind] <= tar:
+            pdp[nums[ind]] = True
+
+        for ind in range(1, n):
+            curr = pdp[:]
+            # curr[0] = True
+            for tar in range(1, target+1):
+                dontTake = pdp[tar]
+                take = False
+                if tar >= nums[ind]:
+                    take = pdp[tar-nums[ind]]
+                curr[tar] = dontTake or take
+            pdp = curr
+        return pdp[target]
+        
