1281c394c5ef5aa2d4526064ff3da262e18a9f7d
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-04 12:21:27 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,35 @@
+class DSU:
+    def __init__(self, n):
+        self.n = n
+        self.par = [i for i in range(n)]
+        self.size = [1 for i in range(n)]
+    
+    def find(self, node):
+        if self.par[node] != node:
+            self.par[node] = self.find(self.par[node])
+        return self.par[node]
+
+    def unionBySize(self, u, v):
+        uPar = self.find(u)
+        vPar = self.find(v)
+        if uPar == vPar:
+            return False
+        if self.size[uPar] >= self.size[vPar]:
+            self.size[uPar] += 1
+            self.par[vPar] = uPar
+        else:
+            self.size[vPar] += 1
+            self.par[uPar] = vPar
+        return True
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        for u, v in edges:
+            dsu.unionBySize(u, v)
+        ans = 0
+        for i in range(n):
+            if i == dsu.find(i):
+                ans += 1
+        return ans
+        
