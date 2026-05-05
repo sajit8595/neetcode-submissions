f0e8a975e8ed428051084d7901ee3397e29f36d0
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-05 10:30:23 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean checkIfPossible(int mid, int[] piles, int h) {
+        int currH = 0;
+        for (int i = 0; i < piles.length; i++) {
+            int reqH = (piles[i] + mid - 1) / mid;
+            currH += reqH;
+            if (currH > h) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = Arrays.stream(piles).max().getAsInt();
+        int ans = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (checkIfPossible(mid, piles, h)) {
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
