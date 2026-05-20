4a0161622ccc02de576391d4568142e52d79a610
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-20 22:35:26 +0530

    Add: interleaving-string - submission-4

---
+++ Data Structures & Algorithms/interleaving-string/submission-4.py
@@ -0,0 +1,52 @@
+class Solution:
+    def __init__(self):
+        self.s1 = ''
+        self.s2 = ''
+        self.s3 = ''
+
+        self.s1Len = 0
+        self.s2Len = 0
+        self.s3Len = 0
+
+        self.memo = {}
+
+
+    def recursion(self, ind1, ind2):
+        # print('recur')
+        ind3 = (ind1 + ind2)
+        if ind3 == self.s3Len:
+            return ind1 == self.s1Len and ind2 == self.s2Len
+        
+        key = (ind1, ind2)
+        if key in self.memo:
+            return self.memo[key]
+
+        if ind1 < self.s1Len and self.s1[ind1] == self.s3[ind3]:
+            # print(ind1, ind3, self.s1[ind1], self.s3[ind3])
+            recurInd1 = self.recursion(ind1+1, ind2)
+            if recurInd1:
+                self.memo[key] = True
+                return self.memo[key]
+        
+        if ind2 < self.s2Len and self.s2[ind2] == self.s3[ind3]:
+            # print(ind2, ind3, self.s2[ind2], self.s3[ind3])
+            recurInd2 = self.recursion(ind1, ind2+1)
+            if recurInd2:
+                self.memo[key] = True
+                return self.memo[key]
+
+        self.memo[key] = False
+        return self.memo[key]
+
+
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        
+        self.s1Len = len(s1)
+        self.s2Len = len(s2)
+        self.s3Len = len(s3)
+
+        # print('ajit')
+        return self.recursion(0, 0)
