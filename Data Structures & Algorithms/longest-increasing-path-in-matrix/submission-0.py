77d150500573189e710af58ea2d7fc85eea8d590
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-05-24 17:42:55 +0530

    Add: longest-increasing-path-in-matrix - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestIncreasingPath(self, mat: List[List[int]]) -> int:
+        n, m = len(mat), len(mat[0])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        memo = {}
+        def recur(i, j):
+            ans = 1
+            key = (i, j)
+            if key in memo:
+                return memo[key]
+
+            for xi, yi in dirs:
+                ni, nj = i + xi, j + yi
+                if ni >= 0 and nj >= 0 and ni < n and nj < m and mat[ni][nj] > mat[i][j]:
+                    ans = max(ans, 1 + recur(ni, nj))
+            memo[key] = ans
+            return memo[key]
+
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                ans = max(ans, recur(i, j))
+        return ans
