6485b78d2e5bdcba9cca3381c06ba930c1d3e833
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-06-22 16:36:22 +0530

    Add: single-element-in-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        low = 0
+        high = n-1
+
+        # 0, 1, 2, 3
+        # 1st ele on even, then 2nd on odd
+
+        while low < high:
+            mid = (low + high) // 2
+            if mid % 2 == 0:
+                mid += 1
+
+            if nums[mid-1] == nums[mid]:
+                low = mid+1
+            else:
+                high = mid-1
+        
+        return nums[low]
