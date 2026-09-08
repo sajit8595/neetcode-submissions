3f934a724e8a56f2b44b2b89929bd5e5d2caad61
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-09-08 19:38:22 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+        memo = {}
+
+        n = len(s)
+        def recur(ind, prevInd):
+            if ind == n:
+                return s[prevInd:ind+1] in wordSet
+            
+            key = (ind, prevInd)
+            if key in memo:
+                return memo[key]
+
+            op1 = False
+            if s[prevInd:ind] in wordSet:
+                op1 = recur(ind, ind)
+            op2 = recur(ind+1, prevInd)
+
+            memo[key] = op1 or op2
+            return memo[key]
+        
+        return recur(0, 0)
