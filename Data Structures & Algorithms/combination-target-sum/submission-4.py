89f32079e88cb1c5540d5625650a0dfc060c640a
Ajit Singh <66949427+sajit8595@users.noreply.github.com>
2026-09-09 21:29:55 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        nums.sort()
+        n = len(nums)
+        ans = []
+
+        def recur(ind, path, curTot):
+            if curTot == target:
+                ans.append(path.copy())
+                return
+
+            for nInd in range(ind, n):
+                if curTot + nums[nInd] > target:
+                    return
+
+                path.append(nums[nInd])
+                recur(nInd, path, curTot + nums[nInd])
+                path.pop()
+            
+        recur(0, [], 0)
+
+        return ans
+
